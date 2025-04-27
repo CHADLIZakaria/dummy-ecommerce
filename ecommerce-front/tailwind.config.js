38c1be86ca326ae9d376de0a73ca6e27f081986e
CHADLIZakaria <54622974+CHADLIZakaria@users.noreply.github.com>
2025-04-27 19:18:53 +0100

    landing page (2)

---
+++ ecommerce-front/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'main-color': "#f4a261",
+      }
+    },
+    fontFamily: {
+      sans: [
+        '"Inter"',
+        '"Mouse Memoirs"',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      header: '"Mouse Memoirs"'
+    }
+  },
+  plugins: [],
+}
+
