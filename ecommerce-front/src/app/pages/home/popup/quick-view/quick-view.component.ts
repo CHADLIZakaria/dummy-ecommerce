e836095b1d2dbf77093264343ccc330893877ed3
CHADLIZakaria <54622974+CHADLIZakaria@users.noreply.github.com>
2025-06-18 18:41:52 +0100

    Add  cart and favorite to quick view

--- ecommerce-front/src/app/pages/home/popup/quick-view/quick-view.component.ts
+++ ecommerce-front/src/app/pages/home/popup/quick-view/quick-view.component.ts
@@ -4,19 +4,52 @@ import { QuickViewService } from '../services/quick-view.service';
 import { EcomHelper } from '../../../../shared/helper/ecomHelper';
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from "../../../../shared/components/loading/loading.component";
+import { UserService } from '../../../../shared/services/user.service';
+import { AlertComponent } from '../../../../shared/components/alert/alert.component';
+import { CartItem } from '../../models/home.model';
+import { ProductDetails } from '../../../../shared/model/ecom.model';
 
 @Component({
   selector: 'ecom-quick-view',
-  imports: [NoScrollDirective, CommonModule, LoadingComponent],
+  imports: [NoScrollDirective, CommonModule, LoadingComponent, AlertComponent],
   templateUrl: './quick-view.component.html',
   styleUrl: './quick-view.component.scss'
 })
 export class QuickViewComponent {
   quickViewService = inject(QuickViewService)
+  userService = inject(UserService)
   quickViewDetailsResource = this.quickViewService.quickViewDetailsResource
   currentImage = 0
   numberStar = EcomHelper.range(5)
+  alert = {show: false, message: '', type: ''}
+
   changeCurrentImage(idx: number) {
     this.currentImage = idx;
+  }
+
+  toggleFavorite(idProduct: number) {
+    console.log("fav")
+    this.userService.toggleFavorite(idProduct).subscribe(
+      data => {
+        if(data.status===200) {
+          this.alert = {type: 'favorite', show: true, message: data.data.message}
+          this.quickViewDetailsResource.value().data.isFavorite = ! this.quickViewDetailsResource.value().data.isFavorite          
+        }
+      }
+    )
+  }
+
+  onAddToCart(product: ProductDetails) {
+    const cartItem: CartItem = {
+      productImage: product.coverImage,
+      productName: product.name,
+      price: product.price,
+      quantity: 1
+    }
+    this.userService.addCart(cartItem).subscribe(data => {
+      if(data.status===200) {
+        this.alert = {type: 'cart', show: true, message: data.message}
+      }
+    })
   }
 }
