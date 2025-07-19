0c2d6cd97185b641ccc709b0d9b4c578d008cfba
CHADLIZakaria <54622974+CHADLIZakaria@users.noreply.github.com>
2025-07-19 21:54:10 +0100

    remove spec.ts files

--- ecommerce-front/src/app/shared/pipes/star-count.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StarCountPipe } from './star-count.pipe';
-
-describe('StarCountPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StarCountPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- ecommerce-front/src/app/shared/pipes/star-percentage.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StarPercentagePipe } from './star-percentage.pipe';
-
-describe('StarPercentagePipe', () => {
-  it('create an instance', () => {
-    const pipe = new StarPercentagePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
