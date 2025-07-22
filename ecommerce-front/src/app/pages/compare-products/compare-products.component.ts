ea4e06c00a6ab01b1fe21d821028dea1e09ed9b1
CHADLIZakaria <54622974+CHADLIZakaria@users.noreply.github.com>
2025-07-22 23:02:08 +0100

    Search product in compare product IHM

--- ecommerce-front/src/app/pages/compare-products/compare-products.component.ts
+++ ecommerce-front/src/app/pages/compare-products/compare-products.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SearchProductsComponent } from "./popup/search-products/search-products.component";
 
 @Component({
   selector: 'ecom-compare-products',
-  imports: [],
+  imports: [SearchProductsComponent],
   templateUrl: './compare-products.component.html',
   styleUrl: './compare-products.component.scss'
 })
---
+++ ecommerce-front/src/app/pages/compare-products/popup/search-products/search-products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ecom-search-products',
+  imports: [],
+  templateUrl: './search-products.component.html',
+  styleUrl: './search-products.component.scss'
+})
+export class SearchProductsComponent {
+
+}
