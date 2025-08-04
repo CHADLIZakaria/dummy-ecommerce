ffd7320b522f0eb409b65518315e2c4acb73ffc2
CHADLIZakaria <54622974+CHADLIZakaria@users.noreply.github.com>
2025-08-04 21:30:28 +0100

    product details images

--- ecommerce-front/src/app/pages/home/components/list-products/filters/filter-price/filter-price.component.ts
+++ ecommerce-front/src/app/pages/home/components/list-products/filters/filter-price/filter-price.component.ts
@@ -1,41 +1,64 @@
-import { Component, computed, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Component, effect, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { HomeServices } from '../../../../services/home-services.service';
+import { debounceTime, filter } from 'rxjs';
 
 @Component({
   selector: 'ecom-filter-price',
+  standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './filter-price.component.html',
   styleUrl: './filter-price.component.scss'
 })
 export class FilterPriceComponent {
-  formPrice;
   homeService = inject(HomeServices);
-  minPrice = computed(() => this.homeService.rangePriceResource.value().data.minPrice);
-  maxPrice =  computed(() => this.homeService.rangePriceResource.value().data.maxPrice)
-  
-  constructor(private formBuilder: FormBuilder) {
-    this.formPrice = this.formBuilder.group({
-      minPrice: this.minPrice(),
-      maxPrice: this.maxPrice()
-    })    
-    this.formPrice.valueChanges.subscribe(value => {
-      if(value.minPrice! >= value.maxPrice!) {
-        this.formPrice.patchValue({
-          minPrice: value.minPrice!-10
-        })
+  formBuilder = inject(FormBuilder);
+
+  formPrice!: FormGroup;
+
+  constructor() {
+    effect(() => {
+      const range = this.homeService.rangePriceResource.value().data;
+
+      // Wait until real values are loaded
+      if (
+        range?.minPrice != null &&
+        range?.maxPrice != null &&
+        range.minPrice !== 0 &&
+        range.maxPrice !== 0 &&
+        !this.formPrice
+      ) {
+        this.formPrice = this.formBuilder.group({
+          minPrice: [range.minPrice],
+          maxPrice: [range.maxPrice]
+        });
+
+        this.formPrice.valueChanges.pipe(
+            debounceTime(300),
+            filter(value => value.minPrice != null && value.maxPrice != null)
+          ).subscribe(value => {
+          const min = value.minPrice!;
+          const max = value.maxPrice!;
+
+          // Enforce a minimum range
+          if (min >= max) {
+            this.formPrice.patchValue({ minPrice: max - 10 }, { emitEvent: false });
+            return;
+          }
+
+          if (max - min < 10) {
+            this.formPrice.patchValue({ maxPrice: min + 10 }, { emitEvent: false });
+            return;
+          }
+
+          // Update the filter
+          this.homeService.productFilter.set({
+            ...this.homeService.productFilter(),
+            minPrice: min,
+            maxPrice: max
+          });
+        });
       }
-      else if(value.maxPrice! - value.minPrice! <= 10) {
-        this.formPrice.patchValue({
-          minPrice: value.minPrice!,
-          maxPrice: value.minPrice!+10
-        })
-      }      
-      this.homeService.productFilter.set({
-        ...this.homeService.productFilter(), 
-        minPrice: value.minPrice!, 
-        maxPrice: value.maxPrice!
-      })
-    })
+    });
   }
 }
