1b7cce852d3e9f50d2784f8c9458a60a94f68091
CHADLIZakaria <54622974+CHADLIZakaria@users.noreply.github.com>
2025-08-08 19:00:49 +0100

    Navigate to page product by category(3)

--- ecommerce-front/src/app/shared/navbar/services/navbar.service.ts
+++ ecommerce-front/src/app/shared/navbar/services/navbar.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, resource, signal } from '@angular/core';
-import { environment } from '../../../../environments/environment.development';
-import { Category, EcomPagination, EcomResponse, initCategoryPagination, initProduct, initProducts, Product } from '../../model/ecom.model';
 import { httpResource } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+import { environment } from '../../../../environments/environment.development';
+import { Category, EcomPagination, EcomResponse, initCategoryPagination, initProduct, Product } from '../../model/ecom.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +11,11 @@ export class NavbarService {
 
   constructor() { }
 
-  categoriesResource = resource<EcomResponse<EcomPagination<Category[]>>, {}>({
-    loader: async () => {
-      const categories = await fetch(`${environment.baseUrl}categories?size=0`)
-      return categories.json()
-    },
-    defaultValue: initCategoryPagination
-  })
+  categoriesResource = httpResource<EcomResponse<EcomPagination<Category[]>>>(
+    () => `${environment.baseUrl}categories?size=0`,
+    { defaultValue: initCategoryPagination }
+  )
+  
 
   searchProductResource = httpResource<EcomResponse<EcomPagination<Product[]>>>(
     () => this.keyword()  === '' ? undefined : `${environment.baseUrl}products?name__like=${this.keyword()}`,
